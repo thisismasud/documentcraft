a13e744c4b7db15851cf92ec6b2c05f16137d78a
Masud Parvez <thisismasud1@gmail.com>
2025-11-23 14:45:29 +0600

    initial commit

---
+++ docs/introduction.md
@@ -0,0 +1,17 @@
+---
+title: 'Introduction'
+date: '2024-02-12'
+parent: null
+order: 1
+author: Alex B
+category: General
+tags: ["intro", "getting-started"]
+---
+
+## API Documentation
+Use the Protocol API to access contact, converstaions, group messages, and more seamlessly integrate your product into workflows of dozens of eveoted Protocl users.
+
+### Getting Started
+To get started, create a new application in your [developer settings](/dev-settings), then read about how to make requests for the resources you need to 
+access using our HTTP APIs or dedicated client SDKs. When your integration is ready to go live, publish it to our [integration directory](/integration-directory) to
+react the Protocol community.
---
+++ lib/doc.ts
@@ -0,0 +1,36 @@
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+
+const postDirectory = path.join(process.cwd(), "docs");
+
+interface DocumentData {
+    id: string,
+    title: string,
+    date?: string,
+    parent?: string,
+    order: 3,
+    author?: string,
+    category: string,
+    tags?: string[] 
+}
+
+export function getDocuments() {
+  const fileNames = fs.readdirSync(postDirectory);
+  const allDocuments = fileNames.map((fileName: string) => {
+    const id = fileName.replace(".md", ""); //gets all the ids
+
+    const fullPath = path.join(postDirectory, fileName);
+
+    const fileContents = fs.readFileSync(fullPath, "utf8"); //converting to text format
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...matterResult.data,
+    } as DocumentData;
+  });
+  //return all the documents frontmatter
+  return allDocuments.sort((a, b) => a.order - b.order)
+}
